e363185ecd64f441165f311216c37695d0e3bccb
Chatapp <63429008+chatappchat@users.noreply.github.com>
2021-12-01 08:29:21 -0600

    Create Anti Cheat.lua

---
+++ Anti Cheat.lua
@@ -0,0 +1,17 @@
+--[[
+    Made by dizzywig2000, for public use.
+
+    You may use this script in any way.
+
+    MAKE SURE TO PUT THE CODE IN "ServerScriptService"!
+]]
+
+local req = 4320590 --ID of the Anti-Cheat script, do not change unless you know what you are doing.
+
+local whitelistOptions = { --Options for the whitelist (don't touch if you don't want a whitelist)
+  ["WhitelistEnabled"] = false;
+  ["UsePlayerIdForWhitelist"] = true;
+  ["UserPlayerNameForWhitelist"] = false;
+}
+
+local
